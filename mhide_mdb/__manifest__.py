765435a9e6ae076eb9dbdf8297dbc5b2a83cf941
mathadil <aiot.sa.adil@gmail.com>
2024-08-27 16:17:15 +0000

    Initial Odoo Hide PBO Module

---
+++ mhide_mdb/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Hide Manage Databases',
+    'version': '0.1',
+    'summary': 'Hide Manage Databases',
+    'sequence': -100,
+    'description': """
+        Hide Manage Databases""", 
+    'author': "InnoSA",
+    'website': "#",
+    'category': 'Uncategorized',
+    'license': 'LGPL-3',
+    'images': ['static/description/banner.png'],
+    'depends': ['web'],
+    'data': [
+        'views/views.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
+
